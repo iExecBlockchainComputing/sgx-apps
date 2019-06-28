d5a52c198489c08bc77ce0a73323b2608be9132b
charles de mauroy <charles.demauroy@iex.ec>
2019-06-28 17:24:28 +0200

    nilearn working

--- app/test.py
+++ app/test.py
@@ -1,5 +1,4 @@
-wwith open("/data/dataset.txt", "r") as fin:
-   with open("/scone/result.txt", "w+") as fout:
-       data = fin.read()
-       fout.write(data)
-       print(data)
+with open("/data/dataset.txt", "r") as fin:
+    print("salut!")
+    data = fin.read()
+    print(data)
