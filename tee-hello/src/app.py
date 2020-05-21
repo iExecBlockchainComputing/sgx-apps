e3b8866364d213e2b65bb37edd22ddc2ca17fd01
Zied <26070035+zguesmi@users.noreply.github.com>
2020-05-21 06:58:52 +0200

    Update tee-hello to use $IEXEC_OUT

--- tee-hello/src/app.py
+++ tee-hello/src/app.py
@@ -1,30 +1,32 @@
 import os
 import sys
+import json
 
 print("Started compute tee-hello")
 
-PROTECTED_IEXEC_OUT = "/scone/iexec_out";
+iexec_out = os.environ['IEXEC_OUT']
+result_file = iexec_out + '/result.txt'
+computed_json_file = iexec_out + '/computed.json'
 
-dir = os.path.join(PROTECTED_IEXEC_OUT)
+
+dir = os.path.join(iexec_out)
 if not os.path.exists(dir):
-    os.mkdir(dir)
+    os.makedirs(dir)
 
 IEXEC = ' _\n(_)\n _  _____  _____  ___\n| |/ _ \ \/ / _ \/ __|\n| |  __/>  <  __/ (__\n|_|\___/_/\_\___|\___|\n\n'
 
-with open(PROTECTED_IEXEC_OUT + "/result.txt", "w+") as fout:
-
+with open(result_file, "w+") as fout:
     name = "you"
-
     if len(sys.argv) > 1:
         name = sys.argv[1]
+    result_txt = IEXEC + "hello from an enclave " + name + "\n"
+    fout.write(result_txt)
+    print(result_txt)
 
-    resultTxt = IEXEC + "hello from an enclave " + name + "\n"
-
-    fout.write(resultTxt)
-    print(resultTxt)
-
-
-# touch 'completed-compute.iexec' file at end of compute
-open(PROTECTED_IEXEC_OUT + '/completed-compute.iexec', 'a').close()
+# Return by writting 'computed.json' file at end of compute
+with open(computed_json_file, "w+") as f:
+    computed_json = { "deterministic-output-path" : result_file }
+    json.dump(computed_json, f)
+    print(computed_json)
 
 print("Ended compute tee-hello")
